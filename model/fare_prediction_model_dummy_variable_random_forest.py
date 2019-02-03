0529eb96643047d9b09f669b8223237b3af11f39
Dilli Raj <dillisarathy@gmail.com>
2019-02-03 21:29:21 +0530

    Removes the unwanted line

--- model/fare_prediction_model_dummy_variable_random_forest.py
+++ model/fare_prediction_model_dummy_variable_random_forest.py
@@ -8,9 +8,8 @@ import math
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
-df = pd.read_pickle('./corporate_converted_dummy_variable.pkl')
-#print(df.head().dtypes)
-#sys.exit(1)
+df = pd.read_pickle('../data/corporate_converted_dummy_variable.pkl')
+
 X = df.drop(['origin_airport_code','dest_airport_code','pnr_date','airline_code','flight_no','date_departure','time_departure','base_fare','Cabin class','Trip type','Fare type','departure_timestamp','orgin-dest','requested_timestamp','origin_airport_code_label','dest_airport_code_label','airline_code_label'],axis=1)
 X_train,X_test,y_train,y_test = train_test_split(X, df.base_fare, test_size=0.33, random_state=5)
 regressor = RandomForestRegressor(n_estimators=70, random_state=0)
@@ -19,7 +18,7 @@ y_pred = regressor.predict(X_test)
 
 mse = mean_squared_error(y_pred,y_test)
 mae = mean_absolute_error(y_pred,y_test)
-print("mse",mse) #2868604.735152483
-print("mae",mae) #1396.9930528124487
-print(math.sqrt(mse)) #1693.6955851487844
+print("mse",mse) 
+print("mae",mae) 
+print(math.sqrt(mse)) 
 print("test score",regressor.score(X_test,y_test))
